d795f1ff97c3ca8ea12a7a41db32fd317f358208
Carl Brännström <carlbrannstrom@MacBook-Pro-som-tillhor-Carl.local>
2023-01-31 12:21:29 +0100

    Add BlogPost model

---
+++ Models/BlogPost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DeveloperBlog.Models
+{
+	public class BlogPost
+	{
+		public int Id { get; set; }
+		public string? Title { get; set; }
+		public string? ContentShort { get; set; }
+		public string? ContentFull { get; set; }
+
+	}
+}
+
